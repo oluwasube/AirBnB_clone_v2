7280642625f804eed84c4e9bdbf45315ab6a5531
oluwasube <enibeke20@gmail.com>
2022-10-17 16:05:25 +0100

    0-setup_web_static completed

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Bash script to set up web server
+sudo apt-get update -y
+sudo apt-get -y upgrade
+sudo apt-get install -y nginx
+sudo service nginx start
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+echo "Holberton School" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data
+sudo sed -i '/listen 80 default_server;/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo service nginx restart
