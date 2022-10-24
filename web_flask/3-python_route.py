da82eb47a4d786d197b6572a6b1338cdd7d9b9d1
oluwasube <oluwasubepromise@gmail.com>
2022-10-24 20:15:42 +0100

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""start a flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hnbn():
+    """display Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def show_hnbn():
+    """display HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_c(text):
+    """display C followed by the value of text"""
+    string = text.replace("_", " ")
+    return "C {}".format(string)
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def display_pythontext(text='is cool'):
+    """display Python followed by the value of text"""
+    string = text.replace("_", " ")
+    return "Python {}".format(string)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
